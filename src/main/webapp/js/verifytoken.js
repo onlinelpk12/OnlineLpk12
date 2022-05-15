572756668b16754f4db103a0e50ce9673d3303ad
Siva Kotapati <sivanarasimham.k@gmail.com>
2022-05-15 17:59:49 -0500

    Update verifytoken.js

--- src/main/webapp/js/verifytoken.js
+++ src/main/webapp/js/verifytoken.js
@@ -1,5 +1,7 @@
 	
 	function verifytoken(){
+		
+	if(sessionStorage.getItem("userRole")=="Student"){
 	fetch("https://onlinelpk12node.azurewebsites.net/api/test/student",{
 
 				headers:{
@@ -10,10 +12,10 @@
 				
 				}).then(function(response){
 					var resp=response.json();
-					
+					console.log("hello verify")
 				    if(response.status==200){
 					resp.then((data)=>{		
-							return data;	
+						return true;	
 					})
 	
 				    }
@@ -31,4 +33,43 @@
 
 
     });
+    }
+    else if(sessionStorage.getItem("userRole")=="Teacher"){
+	fetch("https://onlinelpk12node.azurewebsites.net/api/test/teacher",{
+
+				headers:{
+				
+				    'x-access-token':sessionStorage.getItem("token")
+				
 				}
+				
+				}).then(function(response){
+					var resp=response.json();
+					
+				    if(response.status==200){
+					resp.then((data)=>{		
+						return true;	
+					})
+	
+				    }
+				
+				    else{
+				
+				       resp.then((data)=>{
+				    	if(data.message==false){
+				    	sessionStorage.clear();
+				    	location.href="../jsp/login.jsp";	
+				    	}
+				    
+				       })
+				}
+
+
+    });
+    
+}
+else{
+	sessionStorage.clear();
+	location.href="../jsp/login.jsp";	
+}
+}
