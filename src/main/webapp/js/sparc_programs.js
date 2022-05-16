e011dd1fabcab25cabdd35288c41d60e3fe73b33
Siva Kotapati <sivanarasimham.k@gmail.com>
2022-05-16 01:07:13 -0500

    Update sparc_programs.js

--- src/main/webapp/js/sparc_programs.js
+++ src/main/webapp/js/sparc_programs.js
@@ -3,54 +3,54 @@ getSparcProgram(id);
 function getSparcProgram(id) {
 	if(id=="0" || id=="3"){       
 		return `%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-			%% Template for a SPARC file
-			%% Author: 
-			%% Description:
-			%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-			sorts
-			#people = {peter, joaan}.
-			predicates
-			mother (#people, #people).
-			rules
-			mother (joaan, peter).`     
+%% Template for a SPARC file
+%% Author: 
+%% Description:
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+sorts
+#people = {peter, joaan}.
+predicates
+mother (#people, #people).
+rules
+mother (joaan, peter).`     
 	}       
 	else if(id=="1"){       
 		return `%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-			%% Template for a SPARC file
-			%% Author: 
-			%% Description:
-			%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-			sorts
-			#people = {peter, john}.
-			predicates
-			parent(#people, #people).
-			rules
-			parent(john, peter).`
+%% Template for a SPARC file
+%% Author: 
+%% Description:
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+sorts
+#people = {peter, john}.
+predicates
+parent(#people, #people).
+rules
+parent(john, peter).`
 	}
 	else if(id=="2"){       
 		return `%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-			%% Template for a SPARC file
-			%% Author: 
-			%% Description:
-			%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-			sorts
-			#people = {peter, john}.
-			predicates
-			dad(#people, #people).
-			rules
-			dad(john, peter).` 
+%% Template for a SPARC file
+%% Author: 
+%% Description:
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+sorts
+#people = {peter, john}.
+predicates
+dad(#people, #people).
+rules
+dad(john, peter).` 
 	}
 	else{       
 		return `%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-			%% Template for a SPARC file
-			%% Author: 
-			%% Description:
-			%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-			sorts
-			#people = {sara, bob}.
-			predicates
-			father(#people, #people).
-			rules
-			father(bob, sara).`
+%% Template for a SPARC file
+%% Author: 
+%% Description:
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+sorts
+#people = {sara, bob}.
+predicates
+father(#people, #people).
+rules
+father(bob, sara).`
 	}
 }
