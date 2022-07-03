d1bcea52c23a1f0b146550491bd536869f91f7bc
Siva Kotapati <sivanarasimham.k@gmail.com>
2022-07-03 00:44:10 -0500

    Update sparc_programs.js

--- src/main/webapp/js/sparc_programs.js
+++ src/main/webapp/js/sparc_programs.js
@@ -1,7 +1,10 @@
 let id = sessionStorage.getItem("currentLearningOutcomeNumber");
+let lessonNumber = parseInt(sessionStorage.getItem("currentLessonNumber"));
 getSparcProgram(id);
 function getSparcProgram(id) {
-	if(id=="0" || id=="3"){       
+    if(lessonNumber == 2)
+    {   
+	if(id==0 || id==3){       
 		return `%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Template for a SPARC file
 %% Author: 
@@ -14,7 +17,7 @@ mother (#people, #people).
 rules
 mother (joaan, peter).`     
 	}       
-	else if(id=="1"){       
+	else if(id==1){       
 		return `%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Template for a SPARC file
 %% Author: 
@@ -27,7 +30,7 @@ parent(#people, #people).
 rules
 parent(john, peter).`
 	}
-	else if(id=="2"){       
+	else if(id==2){       
 		return `%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Template for a SPARC file
 %% Author: 
@@ -53,4 +56,124 @@ father(#people, #people).
 rules
 father(bob, sara).`
 	}
+}
+else if(lessonNumber == 3)
+{
+    if(id==0)
+    {
+        return `%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        %% Template for a SPARC file
+        %% Author: 
+        %% Description:
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        sorts
+        #people = {peter, joaan}.
+        predicates
+        mother (#people, #people).
+        rules
+        mother (joaan, peter).`;
+    }
+    else if(id == 1)
+    {
+        return `%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        %% Template for a SPARC file
+        %% Author: 
+        %% Description:
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        sorts
+        #people = {peter, John}.
+        predicates
+        father (#people, #people).
+        rules
+        father (John, peter).`;
+    }
+    else if(id==2)
+    {
+        return `%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        %% Template for a SPARC file
+        %% Author: 
+        %% Description:
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        sorts
+        #people = {peter, John}.
+        predicates
+        father (#people, #people).
+        rules
+        father (John, peter).`;
+    }
+    else if(id==3)
+    {
+        return `%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        %% Template for a SPARC file
+        %% Author: 
+        %% Description:
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        sorts
+        #people = {peter, John}.
+        predicates
+        dad (#people, #people).
+        rules
+        dad (John, peter).`;
+    }
+}
+else if(lessonNumber == 4)
+{
+    if(id==0)
+    {
+        `%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        %% Template for a SPARC file
+        %% Author: 
+        %% Description:
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        sorts
+        #people = {peter, John}.
+        predicates
+        parent (#people, #people).
+        rules
+        parent (John, peter).`;
+    }
+    else if(id == 1)
+    {
+        `%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        %% Template for a SPARC file
+        %% Author: 
+        %% Description:
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        sorts
+        #people = {peter, John}.
+        predicates
+        dad (#people, #people).
+        rules
+        dad (John, peter).`;
+    }
+    else if(id==2)
+    {
+        `%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        %% Template for a SPARC file
+        %% Author: 
+        %% Description:
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        sorts
+        #people = {peter, Jooan}.
+        predicates
+        mom (#people, #people).
+        rules
+        mom (Jooan, peter).`;
+    }
+    else if(id==3)
+    {
+        `%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        %% Template for a SPARC file
+        %% Author: 
+        %% Description:
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        sorts
+        #people = {peter, John}.
+        predicates
+        parent (#people, #people).
+        rules
+        parent (John, peter).`;
+    }
+}
+
 }
