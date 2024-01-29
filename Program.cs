e86c662c6f9ecd3d65eb49ce309a25ab2b246cdd
srikargoudkonda <srkonda@ttu.edu>
2024-01-29 09:32:32 -0600

    cors

--- Program.cs
+++ Program.cs
@@ -43,9 +43,9 @@ builder.Services.AddCors(options =>
             builder =>
             {
                 builder.WithOrigins("http://wave.ttu.edu:8080", "http://localhost:8081",
-                    "http://localhost:8082", "http://localhost:8083",
+                    "http://localhost:8082", "http://localhost:8080", "http://localhost:8083",
                     "http://localhost:8084", "http://localhost:8085",
-                    "http://localhost:8086", "http://127.0.0.1:5500",
+                    "http://localhost:8086", "http://localhost:8086", "http://127.0.0.1:5500",
                     "https://127.0.0.1:5500")
                 .AllowAnyMethod()
                 .AllowAnyHeader();
