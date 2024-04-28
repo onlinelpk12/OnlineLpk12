04a2ea39928ee295d6543c7612083b3f12392e69
Bharath Bommala <bbommala@ttu.edu>
2024-04-28 02:50:30 -0500

    CR044 getallassessments API implementation

--- Services/Interface/ILessonAssessmentService.cs
+++ Services/Interface/ILessonAssessmentService.cs
@@ -9,6 +9,7 @@ namespace OnlineLpk12.Services.Interface
         Task<Result<string>> createAFAssessmentData(AFAssessment aFAssessment);
         Task<Result<string>> deleteAFAssessmentData(int courseId, int lessonId, int assessmentId);
         Task<Result<string>> updateAFAssessmentData(int courseId, int lessonId, int assessmentId);
+        Task<Result<List<AFAssessmentData>>> GetAllAssessments(int courseId, int lessonId);
 
     }
 }
